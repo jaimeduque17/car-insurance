87bd8e6edad3b77f94ec83da21110131b9f82e89
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-27 13:19:46 -0500

    the form is created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Header from './components/Header';
+import Form from './components/Form';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header 
-      title='Car Insurance'
-    />
+    <Container>
+      <Header
+        title='Car Insurance'
+      />
+      <FormContainer>
+        <Form />
+      </FormContainer>
+    </Container>
   );
 }
 
